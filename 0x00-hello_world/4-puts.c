715997278afd388d035bf0cad18f868fa9a81f28
senaygebru44 <natisenay4747@gmail.com>
2023-03-16 16:50:43 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line with puts function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	puts("\"programming is like building multilingual puzzle");
+
+	return (0);
+}
