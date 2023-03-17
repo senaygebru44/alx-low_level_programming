76e6f2b469721cac29457944cdaf3da12542ac87
senaygebru44 <natisenay4747@gmail.com>
2023-03-17 15:48:59 -0700

    nxjd

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ else if (ld == 0)
 }
 else
 {
-	printf("Last digit of %d is %d and is less than 6 and not zero\n", n, ld);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 }
 	return (0);
 }
