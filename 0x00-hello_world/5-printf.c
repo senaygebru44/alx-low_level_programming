e762fb1e03ac7772b8a7a615bf86c254a6586471
senaygebru44 <natisenay4747@gmail.com>
2023-03-16 17:12:01 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line using printf function
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art\n");
+	return (0);
+}
