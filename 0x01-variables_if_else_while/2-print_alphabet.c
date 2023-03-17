2c78a4e4427c15d0830c73fbebd4c0a900d2e730
senaygebru44 <natisenay4747@gmail.com>
2023-03-17 16:12:19 -0700

    ndnx

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 97;
+
+	while (n <= 97)
+
+	{
+		putchar('n');
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
+
