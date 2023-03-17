69429b4e1d34a5b049f511c82f6937fb35f4490d
senaygebru44 <natisenay4747@gmail.com>
2023-03-16 18:21:58 -0700

    damn

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  * Return: 0 (success)
  */
 int main(void)
-
+{
 	printf("size of a char: %d byte(s)\n", sizeof(char));
 	printf("size of an int: %d byte(s)\n", sizeof(int));
 	printf("size of a long int: %d byte(s)\n", sizeof(long int));
