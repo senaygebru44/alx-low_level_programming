677d342a5d8e96c13a28e456c4a7367eac1aafc2
senaygebru44 <natisenay4747@gmail.com>
2023-03-19 15:08:49 -0700

    program that prints golden quote -part 3

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - A C program that print a line to the standard error
+ * main - Entry point
  * Return: 1 (success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str1, 59);
 	return (1);
 }
