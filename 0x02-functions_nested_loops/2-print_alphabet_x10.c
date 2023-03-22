abd6ee7c58e6abb9f9c0ddef065fda859c39d22b
senaygebru44 <natisenay4747@gmail.com>
2023-03-21 17:39:34 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,13 @@
 */
 void print_alphabet_x10(void)
 {
-int i;
-int j;
+int i, j;
 for (i = 0; i <= 9; i++)
 {
 for (j = 97; j <= 122; j++)
 {
 	_putchar(j);
 }
+_putchar('\n');
 }
-_putchar(\n);
 }
