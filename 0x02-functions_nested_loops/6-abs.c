9cedd0d17710a9bef419ae328344f28bcc62a6be
senaygebru44 <natisenay4747@gmail.com>
2023-03-21 18:11:21 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _abs - Entry point
+* Description - computes the absolute value of an integer
+* @i: input number as an integer
+* Return: Absolute value
+*/
+int _abs(int i)
+{
+if (i >= 0)
+{
+return (i);
+}
+else
+{
+return (i * -1);
+}
+}
