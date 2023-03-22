86bedc28a6e67a9faeadda55b47dc1286fdb46e5
senaygebru44 <natisenay4747@gmail.com>
2023-03-21 17:19:21 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,11 +4,11 @@
 * Description - print lowercase alphabet 10 times
 * Return: 0
 */
-int print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
 int i;
 int j;
-for (i = 1; i <= 10; i++)
+for (i = 0; i <= 9; i++)
 {
 for (j = 97; j <= 122; j++)
 {
@@ -16,5 +16,4 @@ _putchar(j);
 }
 }
 _putchar('\n');
-return (0);
 }
