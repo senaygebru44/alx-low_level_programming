5d96393b36e5c9d31070025e6b4dcf7d1f59d1e4
senaygebru44 <natisenay4747@gmail.com>
2023-03-23 15:08:32 -0700

    multiplies tow integer

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* mul - multiply 2 numbers
+* @a: the first number
+* @b: the second number
+* Return: the addition of multiplication
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
