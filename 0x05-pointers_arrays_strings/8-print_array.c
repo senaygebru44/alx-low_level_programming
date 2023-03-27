ca8f00f9201d9a4ad9027cdf030ce4cf3058a0e2
senaygebru44 <natisenay4747@gmail.com>
2023-03-27 15:04:22 -0700

    function that print the n element

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,15 +10,13 @@ void print_array(int *a, int n)
 {
 int i;
 
-for (i = 0; i < n; i++)
-{
-if (i == n - 1)
-{
-printf("%d\n", a[i]);
-}
-else
+for (i = 0; i < (n - 1); i++)
 {
 printf("%d, ", a[i]);
 }
+if (i == (n - 1))
+{
+printf("%d", a[n - 1]);
 }
+printf("\n");
 }
