efd1fc7730d9636bbcc3f2ca8a24c5e27b02231b
senaygebru44 <natisenay4747@gmail.com>
2023-03-30 14:01:12 -0700

    string concatenator

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* *_strcat - function that concatenate two strings
+* @src: source
+* @dest: destination
+* Return: dest
+*/
+char *_strcat(char *dest, char *src)
+{
+int a, b;
+
+a = 0;
+while (dest[a] != '\0')
+{
+a++;
+}
+b = 0;
+while (src[b] != '\0')
+{
+dest[a] = src[b];
+a++;
+b++;
+}
+dest[a] = '\0';
+return (dest);
+}
